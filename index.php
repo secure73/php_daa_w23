ae706d344505eec2215dd9e299fd26102c15a580
secure73 <secure73@gmail.com>
2023-08-23 12:02:20 +0100

    second day - till html form post

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
 <?php
-//Wir definieren Variablen in PHP mit $ 
-//immer Variablen mussen camelCase sein : $myVar , $car , $user
-//Wrong :    Niemals nutzt Capitals , numbers , - in dien variable Name.
-//camelCase:   $usersAuto ,   $autoDriver , $hospitalDoctors 
-// Wir Nutzen ALL Capital nur für CONSTANT : $SERVER_KEY
-//immer nutzt ein Name für dein Variable das bedeutung hast. 
-//Wrong :  $xBtz 
-//vermeindem wie möglig nummer in variable : z.B.   $user1 , $deliveryCar56 
+   $username = $_POST['username'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Document</h1>
+
+    <hr>
+    <p>start php</p>
+    <hr>
+
+    <form method="post">
+        <input type="text" name="username">
+        <input type="password" name="password">
+        <button type="submit">login</button>
+    </form>
+
+    <?php
+    $myHtmlElements = "<br><br><hr><span style='background-color:red'>good .!!!! ..... good</span></br>";
+    $myFamilyName = "Khorsandfard";
+    $myFirstName = "Ali";
+
+    $thinkAboutIt = ' Hello Mr $myFirstName $myFamilyName  ';
+
+    
+    echo "it is username user gave:  $username";
 
 
-/*
-Aufgabe:
-1: definiren für sich 5 variablen mit gut und eindeutigen Name. 
-2. geben sie zwei erste variablen ein integer . 
-3. versuchen Sie vier Grundrechnenarten auf diese zwei variablen und zeigen sie ergibness mit echo command
-4. versuchen Sie ergibness am ende Hello World Zeigen. 
+    //echo $thinkAboutIt;
+    
+    ?>
+</body>
 
-beim andere 3 variablen , geben Sie paar hrml tags als Value und Zeigen die in Page. 
-*/ 
-
-
-$mySpanTag = "<br><hr/><span>it is my span</span>";
-
-$myFirstNumber = 5;
-$mySecondNumer = 13; 
-
-echo $myFirstNumber + $mySecondNumer;
-
-echo "<br>";
-$myName = "Ali";
-$myFamilyName = "Khorsandfard";
-echo "Hello World ".$myName ." ". $myFamilyName;
-echo $mySpanTag;
-
-
-
-
+</html>
