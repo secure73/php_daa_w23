505d7f49749b71b1a2769640db20798b11e87dd0
Ali <secure73@gmail.com>
2023-08-30 11:16:29 +0200

    Aufgabe ProductTable Class

---
+++ app/table/ProductTable.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../vendor/easydb/QueryProvider.php');
+
+class ProductTable extends QueryProvider
+{
+    public int $id;
+    //TODO schreben Sie die rest
+
+    public function __construct()
+    {
+        //schreiben Sie die Reste Parent constructor ausrufen
+    }
+
+    //TODO 
+    //schreiben Sie die CRUD functions
+    //selectByName()
+    //selectById()
+    //selectAll()
+    //inset()
+    //update()
+    //delete()
+
+}
